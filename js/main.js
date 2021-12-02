58aa1dd6afcdb52fe0a39417c6a64832992d7b3b
SKuznetzov <s-kuznecov100@rambler.ru>
2021-12-02 20:16:07 +0300

    animate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
-})
+    $('nav a[href^="#"]').click(function(){
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        return false;
+    });
+});
